7a42869c1331cc8bf12cb33674a8a2460e38ce82
alwolny <kontakt@awolny.pl>
2017-10-27 22:16:50 +0200

    added 5 products to be created when the application is started

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
 insert into shop.user_role(id, role, description) VALUES (1, 'ROLE_USER', 'user');
 insert into shop.user_role(id, role, description) VALUES (2, 'ROLE_ADMIN', 'admin');
-insert into shop.user(user_id, email, first_name, last_name, passwort) VALUES (1, 'admin@o2.pl', 'admin', 'admin', 'admin');
+insert into shop.user(user_id, email, last_name, passwort) VALUES (1, 'admin@o2.pl', 'admin', 'admin');
+insert into shop.user_roles(user_user_id, roles_id) VALUES (1, 2);
+
+ insert into shop.products (units_in_order, units_in_stock, brand, category, description, discontinued, image, manufacturer, name, prod_condition, unit_price) values (0, 200, 'Bosch', 'fridge', 'description', false, null, 'Bosch', 'ip340', null, 3400);
+ insert into shop.products (units_in_order, units_in_stock, brand, category, description, discontinued, image, manufacturer, name, prod_condition, unit_price) values (0, 200, 'Samsung', 'cabinet', 'description', false, null, 'Samsung', 'i25', null, 4500);
+ insert into shop.products (units_in_order, units_in_stock, brand, category, description, discontinued, image, manufacturer, name, prod_condition, unit_price) values (0, 200, 'Bosch', 'coolerbox', 'description', false, null, 'Bosch', 'i40', null, 1200);
+ insert into shop.products (units_in_order, units_in_stock, brand, category, description, discontinued, image, manufacturer, name, prod_condition, unit_price) values (0, 200, 'Samsung', 'refrigerator', 'desc', false, null, 'Samsung', 'i30', null, 6500);
+ insert into shop.products (units_in_order, units_in_stock, brand, category, description, discontinued, image, manufacturer, name, prod_condition, unit_price) values (0, 200, 'Bosch', 'accessory', 'desc', false, null, 'Bosch', 'i50', null, 2800);
