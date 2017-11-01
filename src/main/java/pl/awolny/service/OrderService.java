4afdf9e065ed08a51edd3f13ed2104fdb2f964ea
alwolny <kontakt@awolny.pl>
2017-11-01 22:53:14 +0100

    when the order is submitted, OrderItems will be created

--- src/main/java/pl/awolny/service/OrderService.java
+++ src/main/java/pl/awolny/service/OrderService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import pl.awolny.model.CartItem;
 import pl.awolny.model.Order;
+import pl.awolny.model.OrderItem;
 import pl.awolny.model.Product;
 import pl.awolny.model.ShoppingCart;
 import pl.awolny.model.User;
@@ -27,19 +28,22 @@ public class OrderService {
 	@Autowired
 	ShoppingCartService cartService;
 	
+	@Autowired
+	OrderItemService itemService;
+	
 	public void order(){
 		User user = userService.getAuthUser();
-   		List<CartItem> orderItems = new LinkedList<>();
 		List<CartItem> items = user.getCart().getItems();
+		List<OrderItem> orderItems = new LinkedList<>();
 		BigDecimal price = new BigDecimal(0);
 		price = cartService.getTotal(user);
 		for (CartItem item : items) {
             if(item.getAmount()!=0){
-            	orderItems.add(item);
+            	OrderItem oi = new OrderItem(item.getAmount(), item.getProduct());
+            	orderItems.add(oi);
             }
-        }		
+        }
    		Order order = new Order(user, orderItems, price);
-   		System.out.println("total order price" + order.getTotal());
    		orderRepository.save(order);
    		cartService.deleteItemsFromTheCart(user);
 	}
