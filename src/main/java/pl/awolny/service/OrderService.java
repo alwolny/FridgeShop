c04e74845959637f69f182b41b097cfaab2b7d4b
alwolny <kontakt@awolny.pl>
2017-11-02 17:39:08 +0100

    setting units in stock and in order when an order is submitted

--- src/main/java/pl/awolny/service/OrderService.java
+++ src/main/java/pl/awolny/service/OrderService.java
@@ -24,6 +24,8 @@ public class OrderService {
 	
 	@Autowired
 	UserService userService;
+	@Autowired
+	ProductService productService;
 	
 	@Autowired
 	ShoppingCartService cartService;
@@ -32,19 +34,39 @@ public class OrderService {
 	OrderItemService itemService;
 	
 	public void order(){
+		BigDecimal price = new BigDecimal(0);
+		price = cartService.getTotal();
+		List<OrderItem> orderItems = createOrderItems();
+		setUnits(orderItems);
+		createOrder(orderItems, price);
+   		cartService.deleteItemsFromTheCart();
+	}
+	
+	public List<OrderItem> createOrderItems(){
 		User user = userService.getAuthUser();
 		List<CartItem> items = user.getCart().getItems();
 		List<OrderItem> orderItems = new LinkedList<>();
-		BigDecimal price = new BigDecimal(0);
-		price = cartService.getTotal(user);
 		for (CartItem item : items) {
             if(item.getAmount()!=0){
             	OrderItem orderItem = new OrderItem(item.getAmount(), item.getProduct());
             	orderItems.add(orderItem);
             }
         }
+		return orderItems;
+	}
+	
+	public void setUnits(List<OrderItem> orderItems){
+		for(OrderItem item : orderItems){
+			Product product = productService.getProductByProductId(item.getProduct().getProductId());
+			product.setUnitsInOrder(product.getUnitsInOrder()+item.getAmount());
+			product.setUnitsInStock(product.getUnitsInStock()-item.getAmount());
+			productService.save(product);
+		}
+	}
+	
+	public void createOrder(List<OrderItem> orderItems, BigDecimal price){
+		User user = userService.getAuthUser();
    		Order order = new Order(user, orderItems, price);
    		orderRepository.save(order);
-   		cartService.deleteItemsFromTheCart(user);
 	}
 }
